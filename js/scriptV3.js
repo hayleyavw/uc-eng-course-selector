76ab39316a7dec2024a79a52e6729f3f6283f748
Hayley van Waas <hayleyavw@gmail.com>
2015-09-02 22:07:06 +1200

    builds default table for required subjects

--- js/scriptV3.js
+++ js/scriptV3.js
@@ -95,32 +95,45 @@ function watchEngCB() {
 
 function buildDefaultTable() {
     var subject_table = document.getElementById("subject-table");
-
+    // create table body, and append to subject table
     var tableBody = document.createElement("tbody");
-
-    //var tr = document.createElement('tr');
-    //var td = document.createElement('td');
-    //td.appendChild(document.createTextNode("hi"));
-    //tr.appendChild(td);
-    //tableBody.appendChild(tr);
-
     subject_table.appendChild(tableBody);
 
-    // hard coded ENGR100
+    // hard coded ENGR100 row
     var new_row = subject_table.insertRow(1);
     var new_cell = new_row.insertCell(0);
+    new_cell.className = "required";
     var new_text = document.createTextNode("ENGR100");
     new_cell.appendChild(new_text);
     // adjust column span
     document.getElementById("subject-table").rows[1].cells[0].colSpan = 2;
 
-    // add all compulsory courses to table
-    compulsory_courses = rules["All"];
-    for (var i = 2; i < compulsory_courses.length+1; i++) {
+    // add all required courses to table
+    var required_courses = rules["All"].slice(1); // get all courses except ENGR100
+    var sem1 = [];
+    var sem2 = [];
+    semester_1_courses = semester_occurances["Semester 1"];
+    for (var i in required_courses) { // find which semester each course is availiable in
+        if (semester_1_courses.indexOf(required_courses[i]) != -1) {
+            sem1.push(required_courses[i]);
+        } else { // if not in semester 1, we assume it occurs in semester 2
+            sem2.push(required_courses[i]);
+        }
+    }
+
+    // add rows for required courses
+    for (var i = 2; i < sem1.length+2; i++) {
         var new_row = subject_table.insertRow(i);
         var new_cell = new_row.insertCell(0);
-        var new_text = document.createTextNode(compulsory_courses[i-1]);
+        new_cell.className = "required";
+        var new_text = document.createTextNode(sem1[i-2]);
         new_cell.appendChild(new_text);
+        if (sem2.length >= 1) {
+            var new_cell = new_row.insertCell(1);
+            new_cell.className = "required";
+            var new_text = document.createTextNode(sem2.splice(0,1));
+            new_cell.appendChild(new_text);
+        }
     }
 
 }
